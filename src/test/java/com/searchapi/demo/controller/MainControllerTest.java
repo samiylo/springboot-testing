711191e23c4fa664e37f5a4dd7e13f31754e9ae8
Samiylo <skryshu@gmail.com>
2023-02-12 13:09:07 -0600

    Test numbers endpoint

--- src/test/java/com/searchapi/demo/controller/MainControllerTest.java
+++ src/test/java/com/searchapi/demo/controller/MainControllerTest.java
@@ -1,14 +1,24 @@
 package com.searchapi.demo.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,12 +30,23 @@ class MainControllerTest {
     @Test
     void hello() throws Exception{
         mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string("Hello, World"));
 
     }
 
     @Test
-    void processNumber() {
+    void processNumber() throws Exception {
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper.writeValueAsString(numbers);
+
+
+        mockMvc.perform(post("/numbers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(equalTo("Numbers recieved:[1, 2, 3, 4, 5]")));
+
     }
 }
