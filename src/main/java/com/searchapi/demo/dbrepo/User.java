2921ddfff0cb405028109b6b2dd1b8fb029aaca8
Samiylo <skryshu@gmail.com>
2023-02-13 20:27:37 -0600

    Resolved: Missing JPA dependencies

--- src/main/java/com/searchapi/demo/dbrepo/User.java
+++ src/main/java/com/searchapi/demo/dbrepo/User.java
@@ -1,18 +1,20 @@
 package com.searchapi.demo.dbrepo;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     String name;
+
+    @Column(name = "completed")
     private boolean completed;
 
     public User(){};
