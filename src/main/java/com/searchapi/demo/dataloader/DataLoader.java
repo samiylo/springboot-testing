0992304fbc47187e8b253b7c45946350c11ee7f7
Samiylo <skryshu@gmail.com>
2023-02-19 09:35:15 -0600

    Add DataLoader

---
+++ src/main/java/com/searchapi/demo/dataloader/DataLoader.java
@@ -0,0 +1,23 @@
+package com.searchapi.demo.dataloader;
+
+import com.searchapi.demo.dbrepo.User;
+import com.searchapi.demo.dbrepo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public DataLoader(UserRepository userRepository){
+        this.userRepository = userRepository;
+        LoadUsers();
+    }
+
+    private void LoadUsers() {
+        userRepository.save(new User("Samiylo Kryshu", true));
+        userRepository.save(new User("Diesel Jones", true));
+    }
+}
