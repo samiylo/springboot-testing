53c7b80713dbbb18d6d1340e8b686b3029327772
Samiylo <skryshu@gmail.com>
2023-02-19 14:25:30 -0600

    Add load balancer

--- src/test/java/com/searchapi/demo/controller/MainControllerTest.java
+++ src/test/java/com/searchapi/demo/controller/MainControllerTest.java
@@ -3,10 +3,13 @@ package com.searchapi.demo.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -21,6 +24,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@ImportResource({"classpath*:application-context.xml"})
 @AutoConfigureMockMvc
 class MainControllerTest {
 
