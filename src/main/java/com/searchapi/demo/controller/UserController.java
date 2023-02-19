346927b2435caafef8a09c7ba67dc14433431105
Samiylo <skryshu@gmail.com>
2023-02-19 08:39:33 -0600

    Implement custom exception handleing

--- src/main/java/com/searchapi/demo/controller/UserController.java
+++ src/main/java/com/searchapi/demo/controller/UserController.java
@@ -2,8 +2,12 @@ package com.searchapi.demo.controller;
 
 import com.searchapi.demo.dbrepo.User;
 import com.searchapi.demo.dbrepo.UserRepository;
+import com.searchapi.demo.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -22,22 +26,35 @@ public class UserController {
         return userRepository.save(user);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}")//Get user by id
     public User getUserById(@PathVariable Long id) {
-        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        Optional<User> user = userRepository.findById(id);
+
+        if(user.isPresent()) {
+            return  user.get();
+        } else {
+            throw new RuntimeException(" ::: User " + id + " not found ::: ");
+        }
+
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/{id}")//Create user
     public User updateUserById(@PathVariable Long id, @RequestBody User updatedUser) {
-        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException(" ::: User " + id + " not found ::: "));
         user.setName(updatedUser.getName());
         user.setCompleted(updatedUser.isCompleted());
         return userRepository.save(user);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/{id}")//Delete user
     public void deleteUserById(@PathVariable Long id) {
-        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException(" ::: User " + id + " not found ::: "));
         userRepository.delete(user);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleResourceNotFoundException(ResourceNotFoundException exception) {
+        return exception.getMessage();
     }
 }
---
+++ src/main/java/com/searchapi/demo/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.searchapi.demo.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
